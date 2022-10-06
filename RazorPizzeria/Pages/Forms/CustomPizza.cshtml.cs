b4eb6955e89f2a0a87a0916da7619b2853435dd7
DoraNenoy <darja.leonova.it@gmail.com>
2022-10-06 15:17:32 +0300

    Added pages

---
+++ RazorPizzeria/Pages/Forms/CustomPizza.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPizzeria.Pages.Forms
+{
+    public class CustomPizza : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
---
+++ RazorPizzeria/Pages/Pizza.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPizzeria.Pages
+{
+    public class Pizza : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
