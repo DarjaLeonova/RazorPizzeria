7b1d565fc11b250840db4c33dc3a9ffaf3de0988
DoraNenoy <darja.leonova.it@gmail.com>
2022-10-06 15:35:23 +0300

    Done with home page

---
+++ RazorPizzeria/Pages/Checkout/ThankYou.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPizzeria.Pages.Checkout
+{
+    public class ThankYou : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
